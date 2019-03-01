92517329cc23fcef6bf0908b9a00344066cc3d82
Loraine GAULTIER <lgaultie@e1r5p1.42.fr>
2019-03-01 14:25:24 +0100

    correction

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,11 +6,72 @@
 /*   By: lgaultie <lgaultie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 10:41:45 by lgaultie          #+#    #+#             */
-/*   Updated: 2019/02/28 19:43:54 by lgaultie         ###   ########.fr       */
+/*   Updated: 2019/03/01 14:13:36 by lgaultie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+/*
+int		ft_str_size(char * str)
+{
+	//here we need to calculate the size of the final string for malloc
+}
+*/
+
+char	*ft_arg_type(char *type)
+{
+	if (type[0] == 's')
+		return ("char *");
+	return (NULL);
+}
+
+#include <stdio.h>
+char	*ft_analyse(char *format, t_data *data)
+{
+	if (format[0] == 's')
+	{
+		return (va_arg(data->ap, char*));
+	}
+	if (format[0] == 'd')
+		return (ft_itoa(va_arg(data->ap, int)));
+	return (NULL);
+}
+
+int		ft_printf(const char* format, ...) // fid a better name for the chat *
+{
+	char	*buf;
+	int		i;
+	t_data	data;
+	t_data	*p_data;
+
+	i = 0;
+	if(!(buf = ft_memalloc(sizeof(char) * 1000)))
+		return (0);
+	p_data = &data;
+	va_start(data.ap, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%' && i++)
+		{
+			ft_strcat(buf, ft_analyse((char*)&format[i++], p_data));
+			printf("&format[i] : |%s| \nargs size : %zu\n", &format[i], ft_strlen(&format[i]));
+		}
+		buf[ft_strlen(buf)] = format[i];
+		i++;
+	}
+	buf[ft_strlen(buf)] = '\0';
+	ft_putstr(buf);
+	return (0);
+}
+
+
+
+
+
+
+/////////////////////////////////
+/*#include "ft_printf.h"
 
 void	ft_no_percent(const char *format)
 {
@@ -40,4 +101,4 @@ int		main()
 {
 	ft_printf("salut");
 	return (0);
-}
+}*/
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lgaultie <lgaultie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/27 11:23:16 by lgaultie          #+#    #+#             */
-/*   Updated: 2019/02/28 10:40:54 by lgaultie         ###   ########.fr       */
+/*   Updated: 2019/03/01 14:23:01 by lgaultie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int   main()
 {
-    ft_printf("||MON printf|| le pourcentage s = %s, le pourcentage d = %d", s,d);
-    printf("||VRAI printf|| le pourcentage s = %s, le pourcentage d = %d", s,d);
+    ft_printf("||MON printf|| le pourcentage s = %s, le pourcentage d = %d", "salut", 14);
+    printf("||VRAI printf|| le pourcentage s = %s, le pourcentage d = %d", "salut",14);
     return (0);
 }
